c0dd6759b2373a703d0cd8995dcd8f4c8c029307
odin thomas rochmann <odin.rochmann@gmail.com>
2020-10-10 09:18:46 +0200

    feat: add octokit

--- action/label-pr/src/main.ts
+++ action/label-pr/src/main.ts
@@ -1,1 +1,15 @@
-console.log('hello world');
+import * as core from '@actions/core';
+import { getOctokit } from '@actions/github';
+
+const main = async () => {
+
+  const client = getOctokit(core.getInput('token', { required: true }));
+  const commits = await client.pulls.listCommits({
+    repo: core.getInput('repo', { required: true }),
+    owner: core.getInput('owner', { required: true }),
+    pull_number: +core.getInput('pull_number', { required: true }),
+  });
+  console.log(commits);
+};
+
+main();
