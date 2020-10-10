4350fadad637920df5afa4cf8471e486c112ebc7
odin thomas rochmann <odin.rochmann@gmail.com>
2020-10-10 09:48:15 +0200

    fix: workflows

--- action/label-pr/src/main.ts
+++ action/label-pr/src/main.ts
@@ -5,8 +5,10 @@ const main = async () => {
   const client = getOctokit(core.getInput('token', { required: true }));
   const [repo, owner] = core.getInput('repository', { required: true }).split('/');
   const pull_number = +core.getInput('pull_number', { required: true });
+  const options = { repo, owner, pull_number };
+  console.log(options);
   try {
-    const commits = await client.pulls.listCommits({ repo, owner, pull_number });
+    const commits = await client.pulls.listCommits(options);
     console.log(commits);
   } catch (error) {
     core.setFailed(error.message);
