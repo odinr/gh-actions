fc0eedfa97869a45421d54988bc798652f258739
odin thomas rochmann <odin.rochmann@gmail.com>
2020-10-10 09:34:45 +0200

    fix: workflows

--- action/label-pr/src/main.ts
+++ action/label-pr/src/main.ts
@@ -2,15 +2,23 @@ import * as core from '@actions/core';
 import { getOctokit } from '@actions/github';
 
 const main = async () => {
-  console.log(+core.getInput('pull_number', { required: true }));
+  console.log(
+    core.getInput('repo', { required: true }),
+    core.getInput('owner', { required: true }),
+    +core.getInput('pull_number', { required: true })
+  );
 
   const client = getOctokit(core.getInput('token', { required: true }));
-  const commits = await client.pulls.listCommits({
-    repo: core.getInput('repo', { required: true }),
-    owner: core.getInput('owner', { required: true }),
-    pull_number: +core.getInput('pull_number', { required: true }),
-  });
-  console.log(commits);
+  try {
+    const commits = await client.pulls.listCommits({
+      repo: core.getInput('repo', { required: true }),
+      owner: core.getInput('owner', { required: true }),
+      pull_number: +core.getInput('pull_number', { required: true }),
+    });
+    console.log(commits);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 };
 
 main();
