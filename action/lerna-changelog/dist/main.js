0d0342ac202e949730f9d3a15513192cd881284b
odin thomas rochmann <odin.rochmann@gmail.com>
2020-10-13 12:28:48 +0200

    fix: repo

--- action/lerna-changelog/dist/main.js
+++ action/lerna-changelog/dist/main.js
@@ -1,4 +1,23 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -9,6 +28,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const core = __importStar(require("@actions/core"));
 const lerna_changelog_1 = require("lerna-changelog");
 const config_1 = require("./config");
 const util_lerna_packages_1 = require("util-lerna-packages");
@@ -20,11 +40,14 @@ class GG extends lerna_changelog_1.Changelog {
 }
 const main = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const result = yield new GG(config_1.config).createMarkdown();
-        console.log(result);
+        process.env.GITHUB_AUTH = core.getInput('token', { required: true });
+        const tagFrom = core.getInput('tag_from');
+        const tagTo = core.getInput('tag_to');
+        const result = yield new GG(config_1.config).createMarkdown({ tagFrom, tagTo });
+        core.setOutput('markdown', result);
     }
     catch (error) {
-        console.error(error.message);
+        core.setFailed(error.message);
     }
 });
 main();
--- action/lerna-changelog/src/main.ts
+++ action/lerna-changelog/src/main.ts
@@ -1,3 +1,5 @@
+import * as core from '@actions/core';
+
 import { Changelog } from 'lerna-changelog';
 import { config } from './config';
 import { packages } from 'util-lerna-packages';
@@ -12,11 +14,15 @@ class GG extends Changelog {
 
 const main = async () => {
   try {
+    process.env.GITHUB_AUTH = core.getInput('token', { required: true });
+
+    const tagFrom = core.getInput('tag_from');
+    const tagTo = core.getInput('tag_to');
     // @ts-ignore
-    const result = await new GG(config).createMarkdown();
-    console.log(result);
+    const result = await new GG(config).createMarkdown({ tagFrom, tagTo });
+    core.setOutput('markdown', result);
   } catch (error) {
-    console.error(error.message);
+    core.setFailed(error.message);
   }
 };
 
